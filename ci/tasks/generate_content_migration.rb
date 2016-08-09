c40b7880f100feb6cfe238a1d3877ca36bdf3523
Dr Nic Williams <drnicwilliams@gmail.com>
2016-08-09 14:16:48 +1000

    fix generate_content_migration.rb for dingo-secrets

--- ci/tasks/generate_content_migration.rb
+++ ci/tasks/generate_content_migration.rb
@@ -14,10 +14,10 @@ s3 = Fog::Storage.new(provider: 'AWS',
   aws_access_key_id: ENV['AWS_ACCESS_KEY'],
   aws_secret_access_key: ENV['AWS_SECRET_KEY'],
   region: 'ap-southeast-1')
-bucket = s3.directories.get('dingo-postgresql-pivotaltile')
-tiles = bucket.files.all('prefix' => 'dingo-postgresql-')
+bucket = s3.directories.get('dingo-secrets-pivotaltile')
+tiles = bucket.files.all('prefix' => 'dingo-secrets-')
 versions = tiles.inject([]) do |vs, tile|
-  if tile.key =~ /^dingo-postgresql-(.*)\.pivotal$/
+  if tile.key =~ /^dingo-secrets-(.*)\.pivotal$/
     vs << $1
   end
   vs
@@ -25,7 +25,7 @@ end
 
 content_migrations = <<EOS
 ---
-product: dingo-postgresql
+product: dingo-secrets
 installation_schema_version: "1.6"
 to_version: "#{tile_version}"
 migrations:
