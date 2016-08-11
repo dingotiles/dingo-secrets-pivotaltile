607ec1bf598c8438d6f60c4ad3ccf2ce908566b0
Dr Nic Williams <drnicwilliams@gmail.com>
2016-08-11 18:30:57 +1000

    fix var test

--- ci/tasks/upload-stemcell-opsmgr16.sh
+++ ci/tasks/upload-stemcell-opsmgr16.sh
@@ -4,8 +4,8 @@ stemcell_path=$(ls pivnet-stemcell/*vsphere*)
 ls -al ${stemcell_path}
 
 insecure=
-if [[ "${opsmgr_insecure_verification}X" != "X" ]]; then
-  insecure="-k "
+if [[ "${opsmgr_skip_ssl_verification}X" != "X" ]]; then
+  insecure="-k"
 fi
 
 curl -f ${insecure} -u ${opsmgr_username}:${opsmgr_password} \
