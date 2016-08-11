6d00866d992ebdbc7d0aa8b510c1db92f2275054
Dr Nic Williams <drnicwilliams@gmail.com>
2016-08-11 18:26:25 +1000

    upload stemcell to opsmgr16 too

--- ci/tasks/upload-stemcell-opsmgr16.sh
+++ ci/tasks/upload-stemcell-opsmgr16.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+stemcell_path=$(ls pivnet-stemcell/*vsphere*)
+ls -al ${stemcell_path}
+
+insecure=
+if [[ "${opsmgr_insecure_verification}X" != "X" ]]; then
+  insecure="-k "
+fi
+
+curl -f ${insecure} -u ${opsmgr_username}:${opsmgr_password} \
+  "${opsmgr_url}/api/stemcells" -X POST -F "stemcell[file]=@${stemcell_path}"
